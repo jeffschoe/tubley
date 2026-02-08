10b84783103eea7ab72056d261934de7876a86f8
jeffschoe <ratacame@protonmail.com>
2026-02-07 23:05:24 -0500

    CH1: File Storage, L5: Multipart Uploads

---
+++ NOTES.md
@@ -0,0 +1,20 @@
+## Server
+
+## Starting
+
+```bash
+bun run src/index.ts
+```
+
+## SQLite3
+
+### Opening the db
+
+```bash
+sqlite3 tubely.db
+```
+
+### Exiting
+```bash
+.exit
+```
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getInMemoryURL } from "./assets";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -48,6 +49,52 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData();
 
-  return respondWithJSON(200, null);
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError('Thumbnail file missing')
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20; // 10MB, bitshifted
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`
+    );
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  const fileData = await file.arrayBuffer();
+  if (!fileData) {
+    throw new Error("Error reading file data");
+  }
+
+  let video = getVideo(cfg.db, videoId);
+  if(!video) {
+    throw new NotFoundError("Video not found");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError(
+      `User: ${userID} is not authorized to update this video`
+    );
+  }
+
+  videoThumbnails.set(video.id, {
+    data: fileData,
+    mediaType,
+   } satisfies Thumbnail
+  );
+  
+  const urlPath = getInMemoryURL(cfg, videoId);
+  video.thumbnailURL = urlPath;
+  updateVideo(cfg.db, video);
+
+
+  return respondWithJSON(200, video);
 }
